902d766994e7b27bb70800e237b6e1ea0bf6895c
Amedeo Pro <amedeopro@me.com>
2020-04-19 14:35:56 +0200

    aggiunta la possibilità di eliminare un record

--- App.js
+++ App.js
@@ -1,39 +1,31 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button, ScrollView, FlatList } from 'react-native';
+import GoalItem from './components/GoalItem'
+import GoalInput from './components/GoalInput'
 
 export default function App() {
 
-  const [enteredGoal, setEnteredGoal] = useState('')
   const [arrayGoal, setArrayGoal] = useState([])
 
-  const goalInputHandler = (enteredText) => {
-    setEnteredGoal(enteredText);
+  const addGoalHandler = goalTitle => {
+    setArrayGoal([...arrayGoal, { id: Math.random().toString(), value: goalTitle }])
   }
 
-  const addGoalHandler = () => {
-    setArrayGoal([...arrayGoal, { id: Math.random().toString(), value: enteredGoal }])
+  const removeGoalHandler = goalId => {
+    setArrayGoal(currentGoals =>{
+      return currentGoals.filter((goal)=> goal.id !== goalId)
+    })
   }
 
   return (
     <View
       style={styles.screen}>
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Course Goal"
-          style={styles.input}
-          onChangeText={goalInputHandler}
-          value={enteredGoal}
-        />
-        <Button title="ADD" onPress={addGoalHandler} />
-      </View>
+      <GoalInput onAddGoal={addGoalHandler} />
       <FlatList
         keyExtractor={(item, index) => item.id}
         data={arrayGoal}
-        renderItem={itemData => (<View style={styles.listItem}>
-          <Text>
-            {itemData.item.value}
-          </Text>
-        </View>)} />
+        renderItem={itemData => <GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />}
+      />
     </View>
   );
 }
@@ -41,24 +33,5 @@ export default function App() {
 const styles = StyleSheet.create({
   screen: {
     padding: 50
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center'
-  },
-  input: {
-    borderBottomColor: 'black',
-    borderWidth: 1,
-    padding: 10,
-    width: '80%'
-  },
-  listItem: {
-    backgroundColor: '#ccc',
-    padding: 10,
-    borderColor: 'black',
-    borderWidth: 1,
-    marginVertical: 10
   }
-
 });
